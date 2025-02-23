782f1db0b4a3720646df893d547962a5f00d5db2
Bot473Gaming <90602541+Bot473Gaming@users.noreply.github.com>
2025-02-23 17:56:38 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://tungnguyenduong473:SYiGaEaDylHdXlEK@cluster0.2xl7z.mongodb.net//qlsv_dev', {
+        await mongoose.connect('mongodb+srv://tungnguyenduong473:<KEY>@cluster0.2xl7z.mongodb.net//qlsv_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
